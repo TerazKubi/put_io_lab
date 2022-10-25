f68c4efabd4c3031d561db38adcb767b2f512d79
student <student@lab-45-1.cs.put.poznan.pl>
2022-10-25 08:35:10 +0200

    'new_feature_change_2'

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -14,5 +14,13 @@ class Student
         System.out.println(s1.name); 
         System.out.println(s1.name); 
         System.out.println(s1.name); 
+
+        //zmiana 2
+        System.out.println(s1.id); 
+        System.out.println(s1.id); 
+        System.out.println(s1.id); 
+        System.out.println(s1.id); 
+        System.out.println(s1.id); 
+
      } 
 } 
